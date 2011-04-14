954468bdf6df8f281d28db619f71aae1e9cd87e2
Ming-Ting Yao Wei <mwei@lxde.org>
2011-04-15 03:13:31 +0800

    added language switcher

--- info.php
+++ info.php
@@ -1,8 +1,9 @@
 <?php
 $contents = array(
+	"index.php" => "", # Used for redirecting
 	"en" => "English",
 	"zh-tw" => "正體中文",
 	"zh-cn" => "简体中文",
 );
 
-$menu_hidden = array("index");
+$menu_hidden = array("index.php");
