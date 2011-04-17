7a1beca6e907083e6bb73e1b70a14075a1aa5a4e
Ming-Ting Yao Wei <mwei@lxde.org>
2011-04-17 13:18:07 +0800

    fix json reply without callback

--- api/program/index.php
+++ api/program/index.php
@@ -1,10 +1,19 @@
 <?php
-header("Content-type: application/json; charset=utf-8");
-if (!ctype_alnum($_GET['callback']))
+
+if ($_GET['callback'])
 {
-	die("Malformed callback");
+	if (!ctype_alnum($_GET['callback']))
+	{
+		header("400 Bad Request");
+	}
+	header("Content-type: text/javascript; charset=utf-8");
+	echo $_GET['callback'] . '(';
+	include ("program.json.js");
+	echo ')';
+}
+else
+{
+	header("Content-type: text/javascript; charset=utf-8");
+	include ("program.json.js");
 }
 
-echo $_GET['callback'] . '(';
-include ("program.json.js");
-echo ')';
