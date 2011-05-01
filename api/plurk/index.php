708bda1e003b3e5021861d8ee57ca4dd3d9eb4a7
Timothy Guan-tin Chien <timdream@gmail.com>
2011-05-01 09:41:29 +0800

    Add cross-site xhr header (for testing at local)

--- api/plurk/index.php
+++ api/plurk/index.php
@@ -5,6 +5,7 @@ require('./config.inc.php');
 $file = './search.json.js';
 
 header('Content-Type: text/javascript');
+header('Access-Control-Allow-Origin: *');
 
 if (!file_exists($file) || filemtime($file) < time() - 10*60) {
 	$plurks = file_get_contents('http://www.plurk.com/API/PlurkSearch/search?api_key=' . API_KEY . '&query=COSCUP');
