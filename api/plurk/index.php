48377349456a4fb17f4398de7d9f263a31ac75a1
Ming-Ting Yao Wei <medicalwei@gmail.com>
2011-05-01 10:25:07 +0800

    PHP if…else unshortened.

--- api/plurk/index.php
+++ api/plurk/index.php
@@ -11,6 +11,6 @@ if (!file_exists($file) || filemtime($file) < time() - 10*60) {
 	$plurks = file_get_contents('http://www.plurk.com/API/PlurkSearch/search?api_key=' . API_KEY . '&query=COSCUP');
 	print $plurks;
 	if ($plurks) file_put_contents($file, $plurks);
-} {
+} else {
 	readfile($file);
 }
