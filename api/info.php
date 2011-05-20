168bf393c0f757b784132c03025f4796104eb8e1
Timothy Guan-tin Chien <timdream@gmail.com>
2011-05-20 22:51:18 +0800

    Add API documentation in English

--- api/info.php
+++ api/info.php
@@ -1,7 +1,10 @@
 <?php
 $contents = array(
+	"index" => "API Doc",
 	"sponsors" => "Sponsors API",
 	"program" => "Program API",
 	"plurk" => "Plurk API Proxy",
 	"menu" => "Menu API"
 );
+
+$menu_hidden = array("sponsors", "program", "plurk", "menu");
