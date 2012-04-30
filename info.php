2a6fa733cacf71f9c23aaa7df023d91d35ba50e9
Yao Wei (魏銘廷) <mwei@lxde.org>
2012-04-30 12:39:48 +0800

    info.php fixes for offline page.

--- info.php
+++ info.php
@@ -1,11 +1,11 @@
 <?php
 $contents = array(
 	"index.php" => "", # Used for redirecting
-	"offline.html" => "COSCUP 2012",
+	"offline.html" => "", # Used as offline redirection when using appcache
 	"en" => "English",
 	"zh-tw" => "正體中文",
 	"zh-cn" => "简体中文",
 	"api" => "APIs",
 );
 
-$menu_hidden = array("index.php", "api");
+$menu_hidden = array("index.php", "api", "offline.html");
